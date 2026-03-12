c4bb2713db4d418b0307abe19a0a30329787add6
juanpago11 <juanpago11@gmail.com>
2026-03-12 10:32:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -36,8 +36,16 @@ with st.expander('Analizar texto'):
         x=round(blob.sentiment.polarity,2)
         if x > 0.0 and x <=1.0:
             st.write( 'Es un sentimiento Positivo 😊')
-            image=image.open(
+            with open('Cute doggie.json') as source:
+                  animation=json.load(source)
+              st.lottie(animation,width =350)
         elif x >=-1 and x <= 0:
             st.write( 'Es un sentimiento Negativo 😔')
+            with open('Neutral face.json') as source:
+                    animation=json.load(source)
+                st.lottie(animation,width =350)
         else:
             st.write( 'Es un sentimiento Neutral 😐')
+            with open('Sad Emoji.json') as source:
+                    animation=json.load(source)
+                st.lottie(animation,width =350)
