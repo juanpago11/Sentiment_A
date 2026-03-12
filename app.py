08bc628e648835b9308fae28557004b896dcd5b1
juanpago11 <juanpago11@gmail.com>
2026-03-12 10:36:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 from googletrans import Translator
-from treamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie
 import json
 st.title('Análisis de Sentimiento')
 image = Image.open('emoticones.jpg')
